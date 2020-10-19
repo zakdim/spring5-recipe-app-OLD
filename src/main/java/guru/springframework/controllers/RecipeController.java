2666b665ddb92eff9be4faf8724c4808baf61370
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-10-18 22:32:30 -0400

    S11-193 Display a Recipe by ID

---
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -0,0 +1,27 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dmitri on 2020-10-18.
+ */
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+        return "recipe/show";
+    }
+}
+
